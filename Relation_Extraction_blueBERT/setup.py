ed166fdb3660c34c7a89c82b7b1ac3e86cd5e1f6
PhatHuynh_Flex5 <phathuynh.pth@outlook.com>
2023-01-11 00:40:27 +0100

    add setup.py for bert

---
+++ Relation_Extraction_blueBERT/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bert',
+version='0.1',
+description='Blue Packages',
+url='https://github.com/google-research/bert',
+author='Devlin, Jacob and Chang, Ming-Wei and Lee, Kenton and Toutanova, Kristina',
+author_email='',
+license='MIT',
+packages=['bert'],
+zip_safe=False)
